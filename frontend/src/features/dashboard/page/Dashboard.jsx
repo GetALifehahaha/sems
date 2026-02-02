16ab140c02f99b0a782ade960b11df109f48afce
GetALifeHahaha <sencioahlannour@gmail.com>
2026-02-02 18:29:37 +0800

    feat: added toggle frequency buttons

--- frontend/src/features/dashboard/page/Dashboard.jsx
+++ frontend/src/features/dashboard/page/Dashboard.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-import {Header, PageTitle} from '../../../shared'
+import React, {useState} from 'react'
+import {Button, Header, PageTitle} from '../../../shared'
+import FrequencyButton from '../component/FrequencyButton'
 
 const Dashboard = () => {
+
+  const [frequency, setFrequency] = useState('daily')
+
+  const handleFrequency = (value) => {
+    if (value == frequency) {setFrequency('daily'); return;}
+      
+    setFrequency(value);
+  }
+
   return (
     <div className='p-6 flex flex-col'>
       <Header />
-      <PageTitle className="my-8">
+      <PageTitle className="my-8 ml-8">
         Welcome, ABC User
       </PageTitle>
+      
+      <div className='flex gap-2'>
+          <Button text="Daily" className={`pr-18 ${frequency === "daily" && 'bg-primary text-white'}`} onClick={() => handleFrequency('daily')}/>
+          <Button text="Weekly" className={`pr-18 ${frequency === "weekly" && 'bg-primary text-white'}`} onClick={() => handleFrequency('weekly')}/>
+          <Button text="Monthly" className={`pr-18 ${frequency === "monthly" && 'bg-primary text-white'}`} onClick={() => handleFrequency('monthly')}/>
+      </div>
     </div>
   )
 }
