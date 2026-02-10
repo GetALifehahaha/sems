38e351c7f001d4aa78213222439133bc872ff527
GetALifeHahaha <sencioahlannour@gmail.com>
2026-02-10 12:31:22 +0800

    finish the 4 main data to gather

--- frontend/src/features/dashboard/components/MeterDataBlock.jsx
+++ frontend/src/features/dashboard/components/MeterDataBlock.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
+import { allCaps } from '../../../shared/utils/allCaps'
 
 const MeterDataBlock = ({label, labelText, data}) => {
 	return (
-		<div className='flex flex-1 bg-white rounded-2xl p-2 shadow-2xl'>
+		<div className='flex flex-1 bg-white rounded-2xl p-2.5 shadow-2xl'>
 				<h5 className='flex aspect-square h-full justify-center items-center text-lg font-extrabold bg-primary rounded-xl text-block'>
 					{label}
 				</h5>
-				<div className='flex flex-col ml-auto mr-4'>
-					<h5 className='flex-1 text-right self-center text-sm font-semibold text-text/50'>
-						{labelText}
+				<div className='flex flex-col ml-auto p-1'>
+					<h5 className='flex-1 ml-auto self-center text-xs font-semibold text-text/50'>
+						{allCaps(labelText)}
 					</h5>
 					<h5 className='flex-1 text-right self-center text-2xl font-semibold text-secondary'>
-						{data}
+						{data} {label}
 					</h5>
 				</div>
 			</div>
--- frontend/src/features/dashboard/page/Dashboard.jsx
+++ frontend/src/features/dashboard/page/Dashboard.jsx
@@ -11,9 +11,9 @@ import {
 import { cn } from '../../../shared/utils/cn'
 import { capitalize } from '../../../shared/utils/capitalize.js'
 import Style from '../styles/Style'
-// import SampleData from '../data/SampleData.js'
+import { SampleData } from '../data/SampleData.js'
 import { MeterDataBlock } from '../'
-import { Zap } from 'lucide-react'
+import { Smartphone, Zap } from 'lucide-react'
 
 const Dashboard = () => {
 
@@ -30,10 +30,10 @@ const Dashboard = () => {
 			<Header />
 			
 			{/* Frequency Toggle */}
-			<div className='flex gap-2 bg-white p-1 rounded-md w-fit'>
-					<Button text="Daily" className={`font-semibold text-text/50 text-sm rounded-sm py-1 ${frequency === "daily" && 'bg-primary text-white rounded-r-full'}`} onClick={() => handleFrequency('daily')}/>
-					<Button text="Weekly" className={`font-semibold text-text/50 text-sm rounded-sm py-1 ${frequency === "weekly" && 'bg-primary text-white rounded-full'}`} onClick={() => handleFrequency('weekly')}/>
-					<Button text="Monthly" className={`font-semibold text-text/50 text-sm rounded-sm py-1 ${frequency === "monthly" && 'bg-primary text-white rounded-l-full'}`} onClick={() => handleFrequency('monthly')}/>
+			<div className='flex gap-2 bg-white p-1 rounded-xl w-fit shadow-lg'>
+					<Button text="Daily" className={`font-semibold text-text/50 rounded-lg text-sm py-1 ${frequency === "daily" && 'bg-primary text-white'}`} onClick={() => handleFrequency('daily')}/>
+					<Button text="Weekly" className={`font-semibold text-text/50 rounded-lg text-sm py-1 ${frequency === "weekly" && 'bg-primary text-white'}`} onClick={() => handleFrequency('weekly')}/>
+					<Button text="Monthly" className={`font-semibold text-text/50 rounded-lg text-sm py-1 ${frequency === "monthly" && 'bg-primary text-white'}`} onClick={() => handleFrequency('monthly')}/>
 			</div>
 
 			{/* Main Layout */}
@@ -42,28 +42,41 @@ const Dashboard = () => {
 					<div className="basis-1/3 flex flex-row gap-4">
 						{/* Three Musketeers */}
 						<div className='basis-1/3 flex flex-col gap-4'>
-							<MeterDataBlock label="V" labelText="Voltage" data={220} />
-							<MeterDataBlock label="P" labelText="Power" data={10} />
-							<MeterDataBlock label="C" labelText="Current" data={0.045} />
+							<MeterDataBlock label="V" labelText="Voltage" data={SampleData.voltage} />
+							<MeterDataBlock label="P" labelText="Power" data={SampleData.power} />
+							<MeterDataBlock label="C" labelText="Current" data={SampleData.current} />
 						</div>
 
 						{/* The Count */}
-						<div className='flex flex-1 bg-white rounded-2xl shadow-2xl p-4'>
+						<div className='flex flex-col flex-1 bg-white rounded-2xl shadow-2xl p-4 items-start'>
 							{/* Upper Portion */}
-							<div className="">
+							<div className="flex w-full justify-between items-center">
 								{/* Icon */}
 								<div className='rounded-full aspect-square p-4 border border-background bg-white shadow-xl flex justify-center items-center'>
 									<Zap className="text-primary" />	
 								</div>
 
-								
+								<BlockSubtitle className='font-semibold text-text/50 text-xs'>
+									ESTIMATED POWER CONSUMPTION
+								</BlockSubtitle>
 							</div>
 
 							{/* Middle: The Numbers */}
-							<div className=""></div>
+							<div className="p-4 rounded-xl bg-linear-120 from-primary to-secondary shadow-xl h-24 min-w-1/2 ml-auto flex flex-row gap-2 items-end justify-end">
+								<div className='w-1 h-full rounded-4xl bg-white mr-auto'>
+								</div>
+								<h1 className='text-6xl text-white font-bold'>
+									{SampleData.kWh}
+								</h1>
+								<h5 className='text-sm text-white/75 font-semibold mb-1'>
+									kwh
+								</h5>
+							</div>
 
 							{/* Bottom Portion */}
-							<div className=""></div>
+							<div className="ml-auto mt-auto">
+								<h5 className='text-xs font-medium text-text/25'>*Based on the estimated power consumption data measured by SEMS</h5>
+							</div>
 						</div>
 					</div>
 					<div className="flex-1 bg-white rounded-2xl shadow-2xl">
