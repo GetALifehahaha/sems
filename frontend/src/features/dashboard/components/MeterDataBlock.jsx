c33ecc7a336994559d64abb31c2551fcdfe7eb8b
GetALifeHahaha <sencioahlannour@gmail.com>
2026-02-10 11:42:04 +0800

    prep changes 2

--- frontend/src/features/dashboard/components/MeterDataBlock.jsx
+++ frontend/src/features/dashboard/components/MeterDataBlock.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 
-const MeterDataBlock = ({label, data}) => {
+const MeterDataBlock = ({label, labelText, data}) => {
 	return (
 		<div className='flex flex-1 bg-white rounded-2xl p-2 shadow-2xl'>
-				<h5 className='flex aspect-square h-full justify-center items-center text-md font-semibold bg-primary rounded-sm text-block'>
+				<h5 className='flex aspect-square h-full justify-center items-center text-lg font-extrabold bg-primary rounded-xl text-block'>
 					{label}
 				</h5>
-				<h5 className='flex-1 text-right self-center text-xl font-semibold'>
-					{data}
-				</h5>
+				<div className='flex flex-col ml-auto mr-4'>
+					<h5 className='flex-1 text-right self-center text-sm font-semibold text-text/50'>
+						{labelText}
+					</h5>
+					<h5 className='flex-1 text-right self-center text-2xl font-semibold text-secondary'>
+						{data}
+					</h5>
+				</div>
 			</div>
 	)
 }
--- frontend/src/features/dashboard/page/Dashboard.jsx
+++ frontend/src/features/dashboard/page/Dashboard.jsx
@@ -13,6 +13,7 @@ import { capitalize } from '../../../shared/utils/capitalize.js'
 import Style from '../styles/Style'
 // import SampleData from '../data/SampleData.js'
 import { MeterDataBlock } from '../'
+import { Zap } from 'lucide-react'
 
 const Dashboard = () => {
 
@@ -28,23 +29,41 @@ const Dashboard = () => {
 		<div className='p-6 flex flex-col'>
 			<Header />
 			
-			<div className='flex gap-2 bg-background border-4 border-white p-1 rounded-md w-fit'>
-					<Button text="Daily" className={`font-semibold text-text/50 text-sm rounded-sm py-1 bg-background ${frequency === "daily" && 'bg-primary text-white'}`} onClick={() => handleFrequency('daily')}/>
-					<Button text="Weekly" className={`font-semibold text-text/50 text-sm rounded-sm py-1 bg-background ${frequency === "weekly" && 'bg-primary text-white'}`} onClick={() => handleFrequency('weekly')}/>
-					<Button text="Monthly" className={`font-semibold text-text/50 text-sm rounded-sm py-1 bg-background ${frequency === "monthly" && 'bg-primary text-white'}`} onClick={() => handleFrequency('monthly')}/>
+			{/* Frequency Toggle */}
+			<div className='flex gap-2 bg-white p-1 rounded-md w-fit'>
+					<Button text="Daily" className={`font-semibold text-text/50 text-sm rounded-sm py-1 ${frequency === "daily" && 'bg-primary text-white rounded-r-full'}`} onClick={() => handleFrequency('daily')}/>
+					<Button text="Weekly" className={`font-semibold text-text/50 text-sm rounded-sm py-1 ${frequency === "weekly" && 'bg-primary text-white rounded-full'}`} onClick={() => handleFrequency('weekly')}/>
+					<Button text="Monthly" className={`font-semibold text-text/50 text-sm rounded-sm py-1 ${frequency === "monthly" && 'bg-primary text-white rounded-l-full'}`} onClick={() => handleFrequency('monthly')}/>
 			</div>
 
 			{/* Main Layout */}
 			<div className='flex flex-col md:flex-row gap-8 mt-8 h-320 md:h-160'>
 				<div className="flex flex-col gap-8 flex-1">
 					<div className="basis-1/3 flex flex-row gap-4">
+						{/* Three Musketeers */}
 						<div className='basis-1/3 flex flex-col gap-4'>
-							<MeterDataBlock label="V" data={220} />
-							<MeterDataBlock label="P" data={10} />
-							<MeterDataBlock label="C" data={0.045} />
+							<MeterDataBlock label="V" labelText="Voltage" data={220} />
+							<MeterDataBlock label="P" labelText="Power" data={10} />
+							<MeterDataBlock label="C" labelText="Current" data={0.045} />
 						</div>
-						<div className='flex flex-1 bg-white rounded-2xl shadow-2xl'>
-							
+
+						{/* The Count */}
+						<div className='flex flex-1 bg-white rounded-2xl shadow-2xl p-4'>
+							{/* Upper Portion */}
+							<div className="">
+								{/* Icon */}
+								<div className='rounded-full aspect-square p-4 border border-background bg-white shadow-xl flex justify-center items-center'>
+									<Zap className="text-primary" />	
+								</div>
+
+								
+							</div>
+
+							{/* Middle: The Numbers */}
+							<div className=""></div>
+
+							{/* Bottom Portion */}
+							<div className=""></div>
 						</div>
 					</div>
 					<div className="flex-1 bg-white rounded-2xl shadow-2xl">
